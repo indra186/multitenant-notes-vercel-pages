fc367fdd2cf8dae2d0b7e23b87111e826dab60d1
indra186 <134593150+indra186@users.noreply.github.com>
2025-09-15 08:51:01 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
-import '../styles.css'
+import "../styles.css";
+
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
